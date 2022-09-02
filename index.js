b896dd988d44bca08a1092416952612bda5c2541
shinar3@gmail.com <shenarfichman@Siddeeqahs-MacBook-Pro.local>
2022-09-02 18:55:15 -0400

    commit tree da68161a3f5fcd5351b252ac12891c0b01ee96b7

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solutions in this file
+/*
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    
+  }
+
+  return gifts;
+}
+wrapGifts(gifts);
+
+*/
+
+//const cards = ["eva", "gaia", "caleb"];
+//const events = "birthday"
+
+//let myArray = writeCards(cards, events);
+
+function writeCards(cards, events) {
+    const eA = [];
+    
+ for (let i = 0; i < cards.length; i++) {   
+//    console.log(`Thank you, ${cards[i]}, for the wonderful, ${events} gift!`);
+    eA.push(`Thank you, ${cards[i]}, for the wonderful ${events} gift!`);
+     
+ }
+ return eA;
+};
+
+function countDown (posInt) {
+   
+    while (posInt >= 0) {
+    console.log(posInt--)
+}}
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,3 +1,4 @@
+
 const sinon = require( 'sinon' )
 
 describe( 'index.js', () => {
